abcdb8449de34732b783e5f1752d56313920c4f8
alishahwee <alisha.hwee@outlook.com>
2020-10-21 21:24:51 +0000

    Include project and grade info in student search results
    Co-authored-by: RutaTeclai <rutisyear@gmail.com>

--- hackbright_web.py
+++ hackbright_web.py
@@ -15,10 +15,13 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
+    projects = hackbright.get_grades_by_github(github)
+
     html = render_template("student_info.html",
                            first=first,
                            last=last,
-                           github=github)
+                           github=github,
+                           projects=projects)
 
     return html
 
